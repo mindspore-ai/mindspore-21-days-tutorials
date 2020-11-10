456267af2cdd5f51dfd0b1fb657585797400afeb
leonwanghui <wanghui71leon@gmail.com>
2020-11-10 14:44:33 +0800

    Fix some bugs in chapter3 predict code
    Signed-off-by: leonwanghui <wanghui71leon@gmail.com>

--- chapter3/resnet_ascend/resnet50_predict.py
+++ chapter3/resnet_ascend/resnet50_predict.py
@@ -43,10 +43,10 @@ label_list = ["Agaricus双孢蘑菇,伞菌目,蘑菇科,蘑菇属,广泛分布�
 
 
 def _crop_center(img, cropx, cropy):
-    _, y, x = img.shape
+    y, x, _ = img.shape
     startx = x // 2 - (cropx // 2)
     starty = y // 2 - (cropy // 2)
-    return img[:, starty:starty + cropy, startx:startx + cropx]
+    return img[starty:starty + cropy, startx:startx + cropx, :]
 
 
 def _normalize(img, mean, std):
@@ -55,20 +55,20 @@ def _normalize(img, mean, std):
     assert img.dtype != np.uint8
     mean = np.float64(mean.reshape(1, -1))
     stdinv = 1 / np.float64(std.reshape(1, -1))
-    cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    cv2.subtract(img, mean)
-    cv2.multiply(img, stdinv)
+    cv2.cvtColor(img, cv2.COLOR_BGR2RGB, img)
+    cv2.subtract(img, mean, img)
+    cv2.multiply(img, stdinv, img)
     return img
 
 
 def data_preprocess(img_path):
     img = cv2.imread(img_path, 1)
     img = cv2.resize(img, (256, 256))
+    img = _crop_center(img, 224, 224)
     mean = [0.485 * 255, 0.456 * 255, 0.406 * 255]
     std = [0.229 * 255, 0.224 * 255, 0.225 * 255]
     img = _normalize(img.astype(np.float32), np.asarray(mean), np.asarray(std))
     img = img.transpose(2, 0, 1)
-    img = _crop_center(img, 224, 224)
 
     return img
 
